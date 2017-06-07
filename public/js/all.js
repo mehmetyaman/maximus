97fee606b1e98ee0df4b2ef67795c24a8d2c03ba
kaplanerbil <kaplanerbil@gmail.com>
2017-06-07 05:16:26 +0300

    translator dev.

--- public/js/all.js
+++ public/js/all.js
@@ -6,3 +6,13 @@ function cancelAdd(){
     
     window.location.href = '/customers';
 }
+
+function addTranslator(){
+
+    window.location.href = '/translators/add';
+}
+function cancelAddTranslator(){
+
+    window.location.href = '/translators';
+}
+
