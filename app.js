8b9d66dafd879cb43d846387b58cb0f5aeca2eb1
Mehmet Yaman <mehmetyaman@Mehmet-MacBook-Pro.local>
2017-06-10 14:33:48 +0300

    just few comments

--- app.js
+++ app.js
@@ -1,4 +1,3 @@
-
 /**
  * Module dependencies.
  */
@@ -13,7 +12,7 @@ var users = require('./routes/users');
 var translators = require('./routes/translators');
 var app = express();
 
-var connection  = require('express-myconnection'); 
+var connection = require('express-myconnection');
 var mysql = require('mysql');
 
 // all environments
@@ -30,48 +29,47 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 // development only
 if ('development' == app.get('env')) {
-  app.use(express.errorHandler());
+    app.use(express.errorHandler());
 }
 
 /*------------------------------------------
-    connection peer, register as middleware
-    type koneksi : single,pool and request 
--------------------------------------------*/
+ connection peer, register as middleware
+ type koneksi : single,pool and request
+ -------------------------------------------*/
 
 app.use(
-    
-    connection(mysql,{
-        
+    connection(mysql, {
+
         host: 'localhost',
         user: 'root',
-        password : '',
-        port : 3306, //port mysql
-        database:'maxsimus'
+        password: '',
+        port: 3306, //port mysql
+        database: 'maxsimus'
 
-    },'pool') //or single
+    }, 'pool') //or single
 
 );
 
 
-
 app.get('/', routes.index);
-
+// user mappings here sss
 app.get('/users', users.list);
 app.get('/users/add', users.add);
 app.post('/users/add', users.save);
 app.get('/users/delete/:id', users.delete_user);
 app.get('/users/edit/:id', users.edit);
-app.post('/users/edit/:id',users.save_edit);
-
+app.post('/users/edit/:id', users.save_edit);
+// user mappings here eee
+// translator mappings here sss
 app.get('/translators', translators.list);
 app.get('/translators/add', translators.add);
 app.post('/translators/add', translators.save);
-app.get('/translators/edit/:id', translators.edit);
-app.post('/translators/edit/:id',translators.save_edit);
 app.get('/translators/delete/:id', translators.delete_translator);
-
+app.get('/translators/edit/:id', translators.edit);
+app.post('/translators/edit/:id', translators.save_edit);
+// translator mapping here eee
 app.use(app.router);
 
-http.createServer(app).listen(app.get('port'), function(){
-  console.log('Express server listening on port ' + app.get('port'));
+http.createServer(app).listen(app.get('port'), function () {
+    console.log('Express server listening on port ' + app.get('port'));
 });
