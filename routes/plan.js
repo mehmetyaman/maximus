39e1732526ceb260f62a2fa792a70d961c1eee13
Mehmet Yaman <mehmetyaman@Mehmet-MacBook-Pro.local>
2017-06-27 15:02:40 +0300

    changes about taking plan

--- routes/plan.js
+++ routes/plan.js
@@ -1,16 +1,47 @@
 module.exports = function (app, winston) {
 
-    app.post('/plan', function (req, res, next) {
+    app.post('/plan', isLoggedIn, function (req, res, next) {
         var input = JSON.parse(JSON.stringify(req.body));
 
-        console.log(input);
-    });
-};
+        req.getConnection(function (err, connection) {
 
-// route middleware to ensure user is logged in
+            var data = {
+                lang1: getLang(input.lang1),
+                lang2: getLang(input.lang2),
+                start_date: new Date(input.plandate),
+                end_date: new Date(input.plandate),
+                duration: input.interval
+            }
+            if (err) {
+                console.log("Error inserting : %s ", err);
+            }
+            var query2 = connection.query("INSERT INTO `translation_session` set ?", [data], function (err2, rows2) {
+
+                if (err2) {
+                    console.log("Error inserting : %s ", err2);
+                }
+            });
+
+            console.log(input);
+        });
+    })
+}
+
 function isLoggedIn(req, res, next) {
     if (req.isAuthenticated())
         return next();
 
     res.redirect('/');
+}
+
+function getLang(param) {
+
+    if ("Turkish" == param) {
+        return "tr";
+    } else if ("English" == param) {
+        return "en";
+    } else if ("Japanese" == param) {
+        return "jp";
+    }
+
 }
