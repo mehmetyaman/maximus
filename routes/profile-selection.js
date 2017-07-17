2180599f34540dc22f024d77aa3a03073249371e
Mehmet Yaman <mehmetyaman@Mehmet-MacBook-Pro.local>
2017-07-17 15:55:45 +0300

    linkedin profile changes continue...

--- routes/profile-selection.js
+++ routes/profile-selection.js
@@ -5,26 +5,39 @@ module.exports = function (app) {
     });
 
     app.post('/profile/select', isLoggedIn, function (req, res, next) {
-        var selectedProfile = req.body.selectedProfile;
+        var selectedProfile = req.body['profile-selection'];
         var linkedinprofile = req.session.linkedinprofile;
-
+        var is_customer = selectedProfile === "acustomer" ? 1 : 0;
+        var is_translator = selectedProfile === "atranslator" ? 1 : 0;
         //  is_customer | is_translator | is_linkedin_user
         req.getConnection(function (err, connection) {
             var query = connection.query("update users set is_translator=?,is_customer=?, is_linkedin_user=? where" +
                 " linkedin_id=? ",
-                [selectedProfile === "atranslator" ? 1 : 0, selectedProfile === "acustomer" ? 1 : 0, 1,
-                    linkedinprofile.id], function (err, results, rows) {
+                [is_translator, is_customer, 1, linkedinprofile.id], function (err, results, rows) {
                     if (err) {
                         console.log("Error Updating : %s ", err);
-                        res.send({redirect: '/'});
+                        res.redirect('/');
                     }
 
                     if (selectedProfile === "atranslator") {
                         // cont as a translator
-                        res.send({redirect: '/profilet'});
+
+                        var query = connection.query('select * from languages', function (err1, rows1) {
+
+                            if (err)
+                                console.log("Error Selecting : %s ", err);
+
+                            res.render('signup.ejs', {
+                                message: req.flash('signupMessage'),
+                                customer: false,
+                                linkedinprofile: linkedinprofile,
+                                dataLang: rows1
+                            });
+
+                        });
                     } else if (selectedProfile === "acustomer") {
                         // cont as a customer
-                        res.send({redirect: '/profile'});
+                        res.redirect('/profile');
                     }
 
                 });
