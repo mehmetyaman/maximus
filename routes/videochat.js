d645dc03736266e6e17e60511cc9c01e624cc773
unknown <kaplanerbil@yahoo.com>
2017-10-08 00:56:49 +0300

    videoConference fix

--- routes/videochat.js
+++ routes/videochat.js
@@ -3,53 +3,64 @@ var Peer = require('../app/models/videochatpeer')
 var config = require('config')
 
 module.exports = function (app) {
-  app.get('/video-conference/:id', function (req, res, next) {
-    var videoChatId = req.params.id
-    var peerId = req.user.id
-    var username = req.user.email
-    // var videoChat
-    // var isRoomCreated = false
-    VideoChat.find({'_id': videoChatId}, function (err, videoChat) {
-      if (err) return next(err)
+  app.get('onceki/video-conference/:id', function (req, res, next) {
+        var videoChatId = req.params.id
+        var peerId = req.user.id
+        var username = req.user.email
+        // var videoChat
+        // var isRoomCreated = false
+        VideoChat.find({'_id': videoChatId}, function (err, videoChat) {
+            if (err) return next(err)
 
-      /*
-       if (videoChat.length==0) {
-       createVideoChat(videoChatId, function (err) {
-       if (err) {
-       return next(err)
-       }else{
-       isRoomCreated=true
-       }
-       })
-       }
-       Peer.find({"videoChatId": videoChatId, "id": peerId}, function (err, peer) {
-       if (peer.length==0) {
-       createPeer(videoChatId, peerId, username, function () {
-       if (err) {
-       return next(err)
-       }
-       })
-       }else{
-       console.log("Error /videoChat/:id : %s ", err)
-       res.status(500).send("You already have an active session!")
-       }
-       })
-       */
-      Peer.find({'videoChatId': videoChatId, 'id': {$nin: [peerId]}},
-        function (err, peers) {
-          if (err) return next(err)
-          res.render('trans_session/video-conference', {
+            /*
+             if (videoChat.length==0) {
+             createVideoChat(videoChatId, function (err) {
+             if (err) {
+             return next(err)
+             }else{
+             isRoomCreated=true
+             }
+             })
+             }
+             Peer.find({"videoChatId": videoChatId, "id": peerId}, function (err, peer) {
+             if (peer.length==0) {
+             createPeer(videoChatId, peerId, username, function () {
+             if (err) {
+             return next(err)
+             }
+             })
+             }else{
+             console.log("Error /videoChat/:id : %s ", err)
+             res.status(500).send("You already have an active session!")
+             }
+             })
+             */
+            Peer.find({'videoChatId': videoChatId, 'id': {$nin: [peerId]}},
+                function (err, peers) {
+                    if (err) return next(err)
+                    res.render('trans_session/video-conference', {
+                        title: res.__('Video Conference'),
+                        roomId: videoChatId,
+                        peerId: peerId,
+                        peers: peers,
+                        username: username,
+                        config: config
+                    })
+                })
+        })
+    })
+  app.get('/video-conference/:id', function (req, res, next) {
+        var videoChatId = req.params.id
+        var peerId = req.user.id
+        var username = req.user.email
+        res.render('trans_session/video-conference', {
             title: res.__('Video Conference'),
             roomId: videoChatId,
             peerId: peerId,
-            peers: peers,
             username: username,
             config: config
-          })
         })
     })
-  })
-
   app.get('/peertest/:sessionId', function (req, res) {
     var sessionId = req.params.sessionId
     console.log(sessionId)
